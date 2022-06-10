1cfa33508461c82f4349bb9615f01f22aa036672
Ivan Medina <ivansk8.imm@gmail.com>
2022-06-10 08:26:40 -0500

    fix: bug

--- src/main.sh
+++ src/main.sh
@@ -8,7 +8,7 @@ main(){
     ensure::env_var_exists "GITHUB_REPOSITORY"
     ensure::env_var_exists "GITHUB_EVENT_PATH"
     ensure::total_args 3 "$@"
-    export GITHUB_TOKEN ="$1"
+    export GITHUB_TOKEN="$1"
     ensure::check "$2" "$3"
     exit $?
 
